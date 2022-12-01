ee939ca458fe852181f99fc8c4fa0e4b090f051c
Nadhif Rafifaiz K <nadhifrafifaiz@gmail.com>
2022-12-01 22:30:56 +0700

    Setup Backend

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import multer from 'multer'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }))
+app.use(morgan('common'))
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+
+// MONGOOSE SETUP
+const PORT = process.env.PORT || 6001
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => { console.log(`server port: ${PORT}`) })
+}).catch((error) => {
+    console.log(`${error} did not connect`)
+})
